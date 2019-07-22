04e38a43e96eee49c8dee0292534de67028be7b5
ekkirizki <ekkirizki@gmail.com>
2019-07-22 15:30:21 +0700

    perbaikan

--- app/src/main/java/com/example/yuklesguru/ProfileFragment.java
+++ app/src/main/java/com/example/yuklesguru/ProfileFragment.java
@@ -1,41 +1,34 @@
 package com.example.yuklesguru;
 
-<<<<<<< HEAD
+
 import android.content.Intent;
-=======
->>>>>>> f6dadc35de2c6fddc79090798ca924f2039e5120
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-<<<<<<< HEAD
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-=======
->>>>>>> f6dadc35de2c6fddc79090798ca924f2039e5120
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-<<<<<<< HEAD
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileFragment extends Fragment implements View.OnClickListener {
-=======
-public class ProfileFragment extends Fragment {
->>>>>>> f6dadc35de2c6fddc79090798ca924f2039e5120
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-<<<<<<< HEAD
 
         FirebaseAuth mAuth;
         FirebaseUser mUser;
@@ -73,8 +66,4 @@ public class ProfileFragment extends Fragment {
         startActivity(a);
         getActivity().finish();
     }
-=======
-        return view;
-    }
->>>>>>> f6dadc35de2c6fddc79090798ca924f2039e5120
 }
